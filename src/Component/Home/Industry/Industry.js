8156ee034fe76674227def0c1d26c1fb1437eed9
Abhishek Hazra <abhishek.hazra@codeclouds.com>
2025-08-05 18:09:48 +0530

    Industries Changes Done

--- src/Component/Home/Industry/Industry.js
+++ src/Component/Home/Industry/Industry.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { industryData } from "../../../Data/Home/industry";
 const Industry = () => {
   return (
     <div className="industrySection">
@@ -7,29 +7,22 @@ const Industry = () => {
         <div className="industryTitle">
           <p>Industries Using Safety Shoes</p>
         </div>
-        <div className="industryDesc">
+        {/* <div className="industryDesc">
           <p>A small Description About Us</p>
-        </div>
+        </div> */}
       </div>
       <div className="industryCardSection">
-        <div className="industryCard">
-          <img src="Assets/Logo/capture.PNG" />
-        </div>
-        <div className="industryCard">
-          <img src="Assets/Logo/capture.PNG" />
-        </div>
-        <div className="industryCard">
-          <img src="Assets/Logo/capture.PNG" />
-        </div>
-        <div className="industryCard">
-          <img src="Assets/Logo/capture.PNG" />
-        </div>
-        <div className="industryCard">
-          <img src="Assets/Logo/capture.PNG" />
-        </div>
-        <div className="industryCard">
-          <img src="Assets/Logo/capture.PNG" />
-        </div>
+        {industryData.map((data, key) => (
+          <div className="industryCard" key={key}>
+            <img src={data.image} alt="img" />
+            <div className="yellowBox">
+              <p>{data.text}</p>
+              <a href={data.link} class="viewMore">
+                VIEW MORE
+              </a>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/Data/Home/industry.js
@@ -0,0 +1,32 @@
+export const industryData=[
+    {
+        text:"Safety shoes for Engineering and Fabrication Industry",
+        image:"Assets/Home/industry/engineering.jpg",
+        link:""
+    },
+    {
+        text:"Safety shoes for Automobile Industry",
+        image:"Assets/Home/industry/automobile.jpg",
+        link:""
+    },
+    {
+        text:"Safety shoes for Aviation Industry",
+        image:"Assets/Home/industry/aviation.jpg",
+        link:""
+    },
+    {
+        text:"Safety shoes for Construction Industry",
+        image:"Assets/Home/industry/construction.jpg",
+        link:""
+    },
+    {
+        text:"Safety shoes for Management & Security",
+        image:"Assets/Home/industry/management.jpg",
+        link:""
+    },
+    {
+        text:"Safety shoes for Meat & Fisheries",
+        image:"Assets/Home/industry/fishery.jpg",
+        link:""
+    },
+]
