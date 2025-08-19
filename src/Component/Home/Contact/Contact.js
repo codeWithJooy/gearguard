f1d805e8accdd3b0a28bf86dd590a1015bcb99c4
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2025-08-19 19:26:41 +0530

    Changes Of Contact Added

--- src/Component/Home/Contact/Contact.js
+++ src/Component/Home/Contact/Contact.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Testimonial from "../Testimonial/Testimonial";
 import Footer from "../../Common/Footer/Footer";
+import { uploadContact } from "../../../actions/contactAction";
+
 const Contact = () => {
+  const [contactForm, setContactForm] = useState({
+    first: "",
+    last: "",
+    email: "",
+    phone: "",
+    message: "",
+    lookingFor: "Safety Shoes",
+    buyingFor: "Self",
+    who: "Consumer",
+    frequency: "One Time",
+  });
+  const handleTextChange = async (e) => {
+    const { name, value } = e.target;
+    setContactForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const submitContact = async () => {
+    let val = await uploadContact(contactForm);
+    setContactForm({
+      first: "",
+      last: "",
+      email: "",
+      phone: "",
+      message: "",
+      lookingFor: "Safety Shoes",
+      buyingFor: "Self",
+      who: "Consumer",
+      frequency: "One Time",
+    });
+    if (val) {
+      alert("Request Submitted Successfully");
+    } else {
+      alert("Something Went Wrong");
+    }
+  };
+
   return (
     <div className="contactSection">
       <div className="contactText">
@@ -19,27 +60,57 @@ const Contact = () => {
       <div className="contactForm">
         <div className="formRowHalf">
           <div className="formRowInput">
-            <input type="text" placeholder="Enter Name" />
+            <input
+              type="text"
+              placeholder="First Name"
+              name="first"
+              value={contactForm.first}
+              onChange={handleTextChange}
+            />
           </div>
           <div className="formRowInput">
-            <input type="text" placeholder="Enter Email" />
+            <input
+              type="text"
+              placeholder="Last Name"
+              name="last"
+              value={contactForm.last}
+              onChange={handleTextChange}
+            />
           </div>
         </div>{" "}
         <div className="formRowHalf">
           <div className="formRowInput">
-            <input type="text" placeholder="Your Phone" />
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              value={contactForm.email}
+              onChange={handleTextChange}
+            />
           </div>
           <div className="formRowInput">
-            <input type="text" placeholder="Your Company" />
+            <input
+              type="text"
+              placeholder="Phone"
+              name="phone"
+              value={contactForm.phone}
+              onChange={handleTextChange}
+            />
           </div>
         </div>
         <div className="formRowHalf">
           <div className="formRowInputFull">
-            <textarea placeholder="Message" rows={10}></textarea>
+            <textarea
+              placeholder="Message"
+              rows={10}
+              name="message"
+              value={contactForm.message}
+              onChange={handleTextChange}
+            ></textarea>
           </div>
         </div>
         <div className="formSend">
-          <button>Send Message</button>
+          <button  onClick={submitContact}>Send Message</button>
         </div>
       </div>
     </div>
